692e7810be5e842718920f347bd5fc01113bbca2
김상우 <kswcks1457@naver.com>
2018-05-02 20:08:48 +0900

    re

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -28,8 +29,22 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+	   res.render('index.html');
+	});
+
+	app.get('/signup', function(req, res){
+	   res.render('signup.html');
+	});
+
+	app.post('/logincheck', function(req, res){
+	   if(req.body.id=="test"){
+	      if(req.body.pw=="test"){
+	         res.send("<h1>WELCOME</h1>");
+	      }
+	   }
+	});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
